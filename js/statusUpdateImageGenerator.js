582526c765f4c104288ba2f029825d87786713d9
Thomas Ngo <thoas015@gmail.com>
2018-08-09 23:04:04 -0400

    Word wrapping working. Starting work on printing String objects in an array

--- js/statusUpdateImageGenerator.js
+++ js/statusUpdateImageGenerator.js
@@ -1,122 +1,133 @@
 (function () {
 
+    let testline = "hello";
+    console.log(testline.substring(0, 3));
+
     /* First Drawing Example */
 
-        // select canvas tag in HTML
-        const myCanvas = document.querySelector('#myCanvas');
+    // select canvas tag in HTML
+    const myCanvas = document.querySelector('#myCanvas');
 
-        // initialize "drawing object" within "myCanvas" canvas
-        const ctx = myCanvas.getContext('2d');
+    // initialize "drawing object" within "myCanvas" canvas
+    const ctx = myCanvas.getContext('2d');
 
-        console.log({ ctx });
+    console.log({ ctx });
 
-        // drawing attributes/image attributes
-        ctx.fillStyle = 'teal'; // changing color of the next "drawing method"
-        ctx.fillRect(0, 0, 300, 150); // pixel start coordinates/size of drawing INSIDE canvas
+    // drawing attributes/image attributes
+    ctx.fillStyle = 'teal'; // changing color of the next "drawing method"
+    ctx.fillRect(0, 0, 300, 150); // pixel start coordinates/size of drawing INSIDE canvas
 
-        /* New Text drawing methods */
+    /* New Text drawing methods */
 
-        // (First text method)
-        ctx.fillStyle = 'black'; // change .fillstyle color of the next "drawing method" FIRST
-        ctx.font = "50px Arial";
-        ctx.fillText("Wuut", 10, 50);
+    // (First text method)
+    ctx.fillStyle = 'black'; // change .fillstyle color of the next "drawing method" FIRST
+    ctx.font = "50px Arial";
+    ctx.fillText("Wuut", 10, 50);
 
-        // Second Text method
-        ctx.fillStyle = 'red'
-        ctx.fillText("Ayyyy", 10, 110);
+    // Second Text method
+    ctx.fillStyle = 'red'
+    ctx.fillText("Ayyyy", 10, 110);
 
-        // Image Data manipulation
-        const imgData = ctx.getImageData(0, 0, 320, 200);
+    // Image Data manipulation
+    const imgData = ctx.getImageData(0, 0, 320, 200);
 
-        console.log({ imgData });
+    console.log({ imgData });
 
-        document.getElementById("image").src = myCanvas.toDataURL();
+    document.getElementById("image").src = myCanvas.toDataURL();
 
 
 
     /* Manipulating drawing with Text Input */
 
-        /* Canvas Variables */
+    /* Canvas Variables */
 
-        const generatedCanvas = document.querySelector('#generatedCanvas');
-        const ctx2 = generatedCanvas.getContext('2d');
-        
-        let bgColorSelection;
-        let submittedText = "";
-        let textLineLength = submittedText.length
-        const maxLineChars = 24;
+    const generatedCanvas = document.querySelector('#generatedCanvas');
+    const ctx2 = generatedCanvas.getContext('2d');
 
-        // Font Styles
-        ctx2.font = "30px Arial";
+    let bgColorSelection;
+    let submittedText = "";
+    // let textLineLength = submittedText.length
+    const maxLineChars = 24;
+    let currentLineYPosition = 80;
 
-        /* Input wiring */
+    // Font Styles
+    ctx2.font = "30px Arial";
 
-        // Status Text Input
+    /* Input wiring */
 
-        
-        const textInput = document.querySelector('#inputStatusText');
-        textInput.addEventListener('keyup', addText);
-     
-        function addText() {
-            submittedText = textInput.value;
-            ctx2.fillStyle = 'red';
-            ctx2.clearRect(0, 0, 400, 150); // clears canvas after every keypress
-            ctx2.fillText(submittedText, 200, 80); // repopulates canvas after every clear with current "submittedText"
-            ctx2.textAlign="center";
-                console.log(submittedText);
+    // Status Text Input
+
+
+    const textInput = document.querySelector('#inputStatusText');
+    textInput.addEventListener('keyup', addText);
+
+    function addText() {
+        console.log(submittedText);
+        submittedText = textInput.value;
+        ctx2.fillStyle = 'red';
+        ctx2.clearRect(0, 0, 400, 150); // clears canvas after every keypress
+        if (submittedText.length > 24) {
+            let line01 = submittedText.substring(0, 24);
+            console.log('For Loop is working');
+            console.log(line01);
+                        /* Step 3 */ currentLineYPosition += 10; // need to differentiate between lines
+        }
+        ctx2.fillText(submittedText, 200, currentLineYPosition); // repopulates canvas after every clear with current "submittedText"
+        ctx2.textAlign = "center";
+        // console.log(submittedText);
+    }
+
+    function refreshText() {
+        ctx2.fillStyle = 'red';
+        ctx2.fillText(submittedText, 200, currentLineYPosition);
+        ctx2.textAlign = "center";
+    }
+
+    // Background Color Input
+
+    const backgroundColorInputForm = document.querySelector('#backgroundColorInputForm');
+
+    backgroundColorInputForm.addEventListener("click", function () {
+        let bgColorData = new FormData(backgroundColorInputForm);
+
+        // Test code
+        let output = "";
+        for (const entry of bgColorData) {
+            console.log(entry[0]);
+            console.log(entry[1]); // what data type is entry[1]?
+            bgColorSelection = entry[1];
+        };
+        // END Test Code
+
+        console.log(bgColorSelection);
+        addBackgroundColor(bgColorSelection);
+    })
+
+    // fill entire canvas with background color
+    // redraw text after this is done.
+    function addBackgroundColor(color) {
+
+        /* TODO: Add other color settings */
+
+        if (color === "black") {
+            ctx2.fillStyle = 'black'; // changing color of the next "drawing method"
+            ctx2.fillRect(0, 0, 400, 150); // pixel start coordinates/size of drawing INSIDE canvas
+            refreshText();
+
+            // ctx2.fillStyle = 'red' /* TODO: Fix these two lines to possibly work with "addText()" */
+            // ctx2.fillText(submittedText, 60, 110);
+
+        } else if (color === "white") {
+            ctx2.fillStyle = 'white'; // changing color of the next "drawing method"
+            ctx2.fillRect(0, 0, 400, 150); // pixel start coordinates/size of drawing INSIDE canvas
+            refreshText();
+            // ctx2.fillStyle = 'red'
+            // ctx2.fillText(submittedText, 60, 110);
         }
 
-        function refreshText(){
-            ctx2.fillStyle = 'red';
-            ctx2.fillText(submittedText, 200, 80);
-            ctx2.textAlign="center";
-        }
+    }
 
-        // Background Color Input
 
-        const backgroundColorInputForm = document.querySelector('#backgroundColorInputForm');
-
-        backgroundColorInputForm.addEventListener("click", function () {
-            let bgColorData = new FormData(backgroundColorInputForm);
-            
-            // Test code
-            let output = "";
-            for (const entry of bgColorData) {
-                console.log(entry[0]);
-                console.log(entry[1]); // what data type is entry[1]?
-                bgColorSelection = entry[1];
-            };
-            // END Test Code
-
-            console.log(bgColorSelection);
-            addBackgroundColor(bgColorSelection);
-        })
-
-        // fill entire canvas with background color
-        // redraw text after this is done.
-        function addBackgroundColor(color) {
-            
-            /* TODO: Add other color settings */
-
-            if (color === "black") {
-                ctx2.fillStyle = 'black'; // changing color of the next "drawing method"
-                ctx2.fillRect(0, 0, 400, 150); // pixel start coordinates/size of drawing INSIDE canvas
-                refreshText();
-
-                // ctx2.fillStyle = 'red' /* TODO: Fix these two lines to possibly work with "addText()" */
-                // ctx2.fillText(submittedText, 60, 110);
-
-            } else if(color === "white"){
-                ctx2.fillStyle = 'white'; // changing color of the next "drawing method"
-                ctx2.fillRect(0, 0, 400, 150); // pixel start coordinates/size of drawing INSIDE canvas
-                refreshText();
-                // ctx2.fillStyle = 'red'
-                // ctx2.fillText(submittedText, 60, 110);
-            }
-
-        }
-
-        
 
 
 
